1e2e86f9b80616f22120a23e4c298d9da38cea2b
CS-SEANSEAH <CS-GINSAYSE.SEAH@ntu.edu.sg>
2024-09-19 15:25:49 +0800

    Combine all controller interactions into one maanger script

---
+++ Assets/Scripts/ControllerInteractionsManager.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Oculus.Interaction;
+using Oculus.Interaction.Input;
+
+public class ControllerInteractionsManager : MonoBehaviour
+{
+    #region Singleton Stuff
+    public static ControllerInteractionsManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+    #endregion
+
+    #region Grabbing Items
+    public List<GameObject> GetItemsGrabbedInHand()
+    {
+        List<GameObject> items = new List<GameObject>();
+
+        foreach (GrabInteractor grabInteractor in GameManager.instance.grabInteractors)
+        {
+            if (grabInteractor.HasSelectedInteractable)
+            {
+                if (!items.Contains(grabInteractor.SelectedInteractable.gameObject)) // in case both hands are holding the same item
+                    items.Add(grabInteractor.SelectedInteractable.gameObject);
+            }
+        }
+
+        return items;
+    }
+
+    #endregion
+
+    #region Dropping Items
+    [Header("Drop Items Variables")]
+    public bool allowDropItems = true;
+    [SerializeField] private float dropInterval = 10;
+    
+    [SerializeField] private AudioSource audioSource_player;
+    [SerializeField] private AudioClip audioClip_sighAfterDrop;
+
+    private float dropTimer = 0;
+
+    void EnableDropTimer()
+    {
+        bool timerOn = false;
+        foreach (GrabInteractor grabInteractor in GameManager.instance.grabInteractors)
+        {
+            if (grabInteractor.HasSelectedInteractable)
+                timerOn = true;
+        }
+
+        if (timerOn)
+            dropTimer -= Time.deltaTime;
+
+        if (dropTimer <= 0)
+        {
+            ActivateItemDrop();
+            dropTimer = dropInterval;
+        }
+    }
+
+    private void ActivateItemDrop()
+    {
+        foreach (GrabInteractor grabInteractor in GameManager.instance.grabInteractors)
+        {
+            grabInteractor.ForceRelease();
+        }
+
+        //audioSource_player.PlayOneShot(audioClip_sighAfterDrop);
+    }
+    #endregion
+
+    #region Vibrating Controllers
+    [Header("Vibrating Controllers Variables")]
+    public bool allowControllerVibration = true;
+    [SerializeField] int vibrationStrength = 255; // 0 - 255
+    [SerializeField] int vibrationFrequency = 2; // lower == more frequent vibration
+
+    public void TriggerVibration(int iteration, int frequency, int strength, OVRInput.Controller controllerSide)
+    {
+        OVRHapticsClip clip = new OVRHapticsClip();
+
+        for (int i = 0; i < iteration; i++)
+        {
+            clip.WriteSample(i % frequency == 0 ? (byte)strength : (byte)0);
+        }
+
+        if (controllerSide == OVRInput.Controller.LTouch) // left controller
+        {
+            OVRHaptics.LeftChannel.Preempt(clip);
+        }
+        else if (controllerSide == OVRInput.Controller.RTouch) // right controller
+        {
+            OVRHaptics.RightChannel.Preempt(clip);
+        }
+    }
+
+    void CheckIfHolding()
+    {
+        foreach (GrabInteractor grabInteractor in GameManager.instance.grabInteractors)
+        {
+            if (grabInteractor.HasSelectedInteractable && grabInteractor.gameObject.GetComponent<ControllerRef>().Handedness == Handedness.Left)
+                TriggerVibration(40, vibrationFrequency, vibrationStrength, OVRInput.Controller.LTouch);
+            else if (grabInteractor.HasSelectedInteractable && grabInteractor.gameObject.GetComponent<ControllerRef>().Handedness == Handedness.Right)
+                TriggerVibration(40, vibrationFrequency, vibrationStrength, OVRInput.Controller.RTouch);
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dropping items
+        if (allowDropItems)
+            EnableDropTimer();
+
+        // Vibrating Controllers
+        if (allowControllerVibration)
+            CheckIfHolding();
+    }
+}
