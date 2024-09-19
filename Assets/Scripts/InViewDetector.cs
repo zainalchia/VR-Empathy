ce858ab181646581d067759d71d74f262b7cfce5
CS-SEANSEAH <CS-GINSAYSE.SEAH@ntu.edu.sg>
2024-09-19 13:51:34 +0800

    InViewDetector to see if object is looked at

---
+++ Assets/Scripts/InViewDetector.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InViewDetector : MonoBehaviour
+{
+    List<GameObject> objsInView = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " is in view");
+
+        if (!objsInView.Contains(other.gameObject))
+            objsInView.Add(other.gameObject);
+
+        FurnitureMoveCheck(other.gameObject, true);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (objsInView.Contains(other.gameObject))
+            objsInView.Remove(other.gameObject);
+
+        FurnitureMoveCheck(other.gameObject, false);
+    }
+
+    void FurnitureMoveCheck(GameObject obj, bool lookingAt)
+    {
+        if (obj.GetComponent<MovingFurniture>() != null)
+        {
+            obj.GetComponent<MovingFurniture>().SetIsLookedAt(lookingAt);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
