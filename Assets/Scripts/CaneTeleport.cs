61b7491c35b560243f5429b1fe8e1334b4654283
rafguy19 <70505093+rafguy19@users.noreply.github.com>
2024-12-06 09:17:43 +0800

    alot of wip stuff

--- Assets/Scripts/CaneTeleport.cs
+++ Assets/Scripts/CaneTeleport.cs
@@ -115,12 +115,12 @@ public class CaneTeleport : MonoBehaviour
                     CheckIfCanMove();
 
                     // move input to a manager script if possible
-                    if (OVRInput.GetDown(OVRInput.Button.One) && !buttonPressed)
+                    if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) && !buttonPressed)
                     {
                         buttonPressed = true;
                         MoveToLocation();
                     }
-                    else if (OVRInput.GetUp(OVRInput.Button.One))
+                    else if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger))
                     {
                         buttonPressed = false;
                     }
