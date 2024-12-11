62b8041b51bb6ae22ebaf5fc59221afefc9da63a
zainal <zainalchia@gmail.com>
2024-12-11 17:29:50 +0800

    outline disappears on grab (not tested)

--- Assets/Scripts/OutlineDisappearOnTouch.cs
+++ Assets/Scripts/OutlineDisappearOnTouch.cs
@@ -6,10 +6,27 @@ using UnityEngine;
 public class OutlineDisappearOnTouch : MonoBehaviour
 {
     [SerializeField] Outline outline;
+    bool inTrigger = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponentInParent<GrabInteractor>() != null)
+        {
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponentInParent<GrabInteractor>() != null)
+        {
+            inTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && inTrigger)
         {
             outline.enabled = false;
         }
