b5c0b8e6aca9280b1a2a6ea69ff963748b0e58db
Rizal <107873449+rizal003@users.noreply.github.com>
2025-11-19 16:16:41 +0800

    Fixed up and complete hawker scene
    1. Voice over 7 plays only after hand is healed
    2. Change to the correct bleeding hand material
    3. Make plaster bigger and position it further from palm
    4. Ensure tray part works after hand is healed
    5. Boss faces you and pass u the tray (u pick up from him)
    6. Add all prompts based on storyboard flow
    7. added delay before it fades out to home scene

--- Assets/Scripts/ScenarioManagers/ScenarioManagerReneeTest.cs
+++ Assets/Scripts/ScenarioManagers/ScenarioManagerReneeTest.cs
@@ -167,6 +167,9 @@ public class ScenarioManagerReneeTest : MonoBehaviour
         // Finally. Faster la serve customer!
         narrationAudioSource.PlayOneShot(narrationAudioClips_1[0]);
         yield return new WaitForSeconds(narrationAudioClips_1[0].length);
+
+        promptManager.ShowPrompt(SceneID.Stall, 0, false, 5f);
+
         // How long you want to make me wait? I wait here very long already.
         narrationAudioSource.PlayOneShot(narrationAudioClips_1[1]);
         yield return new WaitForSeconds(narrationAudioClips_1[1].length);
@@ -216,14 +219,13 @@ public class ScenarioManagerReneeTest : MonoBehaviour
         heartbeatUI.KnifeAccidentFlash();
         yield return new WaitForSeconds(narrationAudioClips_1[5].length);
 
-        MovePLayer();
 
         // Boss scolds player
         narrationAudioSource.PlayOneShot(narrationAudioClips_1[6]);
         Boss.gameObject.SetActive(true);
         yield return new WaitForSeconds(narrationAudioClips_1[6].length);
 
-        promptManager.ShowPrompt(SceneID.Stall, 3); //grab the plaster
+        promptManager.ShowPrompt(SceneID.Stall, 3, false, 6f); //grab the plaster
 
         if (playerTeleport != null)
         {
@@ -245,14 +247,16 @@ public class ScenarioManagerReneeTest : MonoBehaviour
         bandaidContainer.GetComponent<PhysicsGrabbable>().enabled = true;
         bandaidContainer.GetComponent<PlasterContainer>().enabled = true;
 
-        // Wait for player to finish plastering (placeholder)
-        yield return new WaitForSeconds(5f);
+        yield return new WaitUntil(() => GameManager.instance.handHealed); //after bandaid
+        //MovePLayer();
 
         // boss confront
         narrationAudioSource.PlayOneShot(narrationAudioClips_1[7]);
         yield return new WaitForSeconds(narrationAudioClips_1[7].length);
 
         TrayOfFood.SetActive(true);
+        promptManager.ShowPrompt(SceneID.Stall, 4, false, 6f);
+
     }
 
     public void OnPlasterContainerGrabbed()
@@ -384,6 +388,8 @@ public class ScenarioManagerReneeTest : MonoBehaviour
 
         PlayerCloth.SetActive(true);
         PlayerCloth.GetComponent<ForceStayGrabbed>().SetForceGrabActive(true);
+        promptManager.ShowPrompt(SceneID.Stall, 5, false, 6f);
+
         yield return null;
     }
 
@@ -406,6 +412,8 @@ public class ScenarioManagerReneeTest : MonoBehaviour
         //But I need to feed my family, and times are tough, it’s so hard to find work nowadays.
         narrationAudioSource.PlayOneShot(narrationAudioClips_1[11]);
         yield return new WaitForSeconds(narrationAudioClips_1[11].length);
+
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("PastNegativeHome", LoadSceneMode.Single);
     }
     #endregion
