8e3702711d18f24cc0e1d993b83d1f274b1b51f6
claytxnzz <clayplayplay@gmail.com>
2025-12-09 15:12:51 +0800

    Porridge interaction completed

--- Assets/Scripts/Ladle.cs
+++ Assets/Scripts/Ladle.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,18 +5,56 @@ using UnityEngine.Events;
 public class Ladle : MonoBehaviour
 {
     public UnityEvent PouredFood;
-    [SerializeField] GameObject PorridgeInBowl;
+
+    [Header("Objects that activate one-by-one in order")]
+    [SerializeField] private List<GameObject> requiredObjects = new List<GameObject>();
+
+    private int currentIndex = 0;
     private bool PlayOnce = true;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "FoodBowl")
+        if (other.CompareTag("FoodBowl"))
         {
-            PorridgeInBowl.gameObject.SetActive(true);
-            if (PlayOnce)
+            Debug.Log("Bowl has been contacted");
+
+            ActivateNextObject();
+
+            if (PlayOnce && AllObjectsActive())
             {
                 PouredFood.Invoke();
                 PlayOnce = false;
             }
         }
+    }
+
+    private void ActivateNextObject()
+    {
+        // If all objects already activated, do nothing
+        if (currentIndex >= requiredObjects.Count)
+            return;
+
+        GameObject obj = requiredObjects[currentIndex];
+
+        if (obj != null)
+        {
+            obj.SetActive(true);
+            Debug.Log("Activated object: " + obj.name);
+        }
+
+        currentIndex++;
+    }
+
+    private bool AllObjectsActive()
+    {
+        for (int i = 0; i < requiredObjects.Count; i++)
+        {
+            if (requiredObjects[i] == null)
+                return false;
+
+            if (!requiredObjects[i].activeInHierarchy)
+                return false;
+        }
+        return true;
     }
 }
