d5e2cf5bdcaf5017cf4eca33e5b90be3b56e50cc
zainal <zainalchia@gmail.com>
2026-07-01 13:24:19 +0800

    made all prompts in past - stay on screen indefinitely until next action is taken (nd to test)

--- Assets/Scripts/SaneTeleporter.cs
+++ Assets/Scripts/SaneTeleporter.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SaneTeleporter : MonoBehaviour
 {
-	[Header("Refrences")]
+	[Header("References")]
 	[SerializeField] SaneTeleporter nextTeleporter = null;
 
 	[Header("Settings")]
 	[SerializeField] Vector3 smallScale;
 	[SerializeField] Vector3 largeScale;
+
+	[Header("Events")]
+	public UnityEvent OnTeleportHere;
 
 	private bool canTeleportHere = false;
 
@@ -25,6 +29,7 @@ public class SaneTeleporter : MonoBehaviour
 	void MovePlayerHere()
 	{
 		GameManager.instance.ovrCamRig.transform.position = transform.position;
+		OnTeleportHere.Invoke();
         gameObject.SetActive(false);
         if (nextTeleporter != null) nextTeleporter.gameObject.SetActive(true);
     }
